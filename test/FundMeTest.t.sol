87265724d371c8e077d3d356b6a28b4a9fbe3ae4
Vk <kinkarvansh@gmail.com>
2025-07-24 07:31:44 +0000

    added: initial test suite for FundMe contract

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testMinDollarIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18, "Minimum USD should be 5");
+    }
+
+    function testOwnerIsMsgSender() public {
+        assertEq(
+            fundMe.i_owner(),
+            address(this),
+            "Owner should be the contract deployer"
+        );
+    }
+}
